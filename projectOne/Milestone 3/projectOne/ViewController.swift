87217a3fc33f9dc9b24648652d8aa28b36a08b27
Susie Gomez <sugo4237@colorado.edu>
2016-10-11 10:52:29 -0600

    project one  milestone 3

--- projectOne/Milestone 3/projectOne/ViewController.swift
+++ projectOne/Milestone 3/projectOne/ViewController.swift
@@ -11,96 +11,50 @@ import UIKit
 class ViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var dollars: UITextField!
-    @IBOutlet weak var pesos: UITextField!
     @IBOutlet weak var kilometers: UITextField!
     @IBOutlet weak var miles: UITextField!
     @IBOutlet weak var celsius: UITextField!
     @IBOutlet weak var farenheit: UITextField!
     @IBOutlet weak var kilograms: UITextField!
     @IBOutlet weak var pounds: UITextField!
-    
+    @IBOutlet weak var currencyRate: UITextField!
+    @IBOutlet weak var foreignCurrency: UITextField!
     
     func textFieldShouldReturn(textField: UITextField) -> Bool {
-        textField.resignFirstResponder() //should hide the keyboard
+        textField.resignFirstResponder()
+        //should hide the keyboard
         return true
-    }
-    
-    //function that will call other functions depending on which textfield is selected
-    
-    func textFieldShouldEndEditing(textField: UITextField) -> Bool { //calculates as soon as you leave textfield
-        if (textField == dollars) {
-            dollarsToPeso()
-            return true
-            
-        } else if (textField == pesos) {
-            pesosToDollar()
-            return true
-            
-        } else if (textField == kilometers) {
-            kmToMiles()
-            return true
-            
-        } else if (textField == miles) {
-            milesToKm()
-            return true
-            
-        } else if (textField == celsius) {
-            celsiusToFaren()
-            return true
-            
-        } else if (textField == farenheit) {
-            farenToCelsius()
-            return true
-            
-        }
-
-        return false
     }
     
     //function to convert dollars to pesos
     
-    func dollarsToPeso(){
-        var dollarAmount:Float //dollars in dollar field
-        var pesosAmount:Float
+    func updateDollars(){
+        var amount:Float //dollars in dollar field
+        var rate:Float
         
         if dollars.text!.isEmpty { //dollars text field
-            dollarAmount = 0.0 //dollars float variable
+            amount = 0.0 //variable will store input in dollar
         } else {
-            dollarAmount = Float(dollars.text!)!
+            amount = Float(dollars.text!)!
+        }
+        if currencyRate.text!.isEmpty {
+            rate = 0.0
+        } else {
+            rate = Float(currencyRate.text!)!*1
         }
         
-        let xRate = Float(19.3)
-        pesosAmount = xRate*dollarAmount
+       //let origDollar=Int(dollars.text!)
+       let total=amount*rate
         
+        //var finalTotal : Float = 0.0
         
         let currencyFormatter = NSNumberFormatter()
         currencyFormatter.numberStyle=NSNumberFormatterStyle.CurrencyStyle
-        let money = currencyFormatter.stringFromNumber(pesosAmount)
-        pesos.text = money! + " MXN"
-    }
+        let money = currencyFormatter.stringFromNumber(total)
+        foreignCurrency.text = money!
+    }//end of function
     
-    //function to convert pesos to dollars
-
-    func pesosToDollar(){
-        var dollarAmount:Float
-        var pesosAmount:Float
         
-        if pesos.text!.isEmpty { //dollars text field
-            pesosAmount = 0.0 //dollars float variable
-        } else {
-            pesosAmount = Float(pesos.text!)!
-        }
-        
-        let xRate = Float(0.052)
-        dollarAmount = xRate*pesosAmount
-        
-        let currencyFormatter = NSNumberFormatter()
-        //currencyFormatter.currencyCode = MXN
-        currencyFormatter.numberStyle=NSNumberFormatterStyle.CurrencyStyle
-        let money = currencyFormatter.stringFromNumber(dollarAmount)
-        dollars.text = money! + " USD"
-    }
-    
     //function to convert kilometer to miles
     
    func kmToMiles(){
@@ -140,7 +94,6 @@ class ViewController: UIViewController, UITextFieldDelegate {
         
         let speedFormatter = NSNumberFormatter()
         speedFormatter.numberStyle=NSNumberFormatterStyle.DecimalStyle
-        //kilometers.text = speedFormatter.stringFromNumber(kilometerAmount)
         let speed = speedFormatter.stringFromNumber(kilometerAmount)
         kilometers.text = speed! + " km"
     }
@@ -173,17 +126,17 @@ class ViewController: UIViewController, UITextFieldDelegate {
         var farenAmount:Float
         
         if farenheit.text!.isEmpty {
-            farenAmount = 0
+            farenAmount = 0.0
         } else {
             farenAmount = Float(farenheit.text!)!
         }
         
         let conversion = Float(0.5556)
-        celsiusAmount = (farenAmount - 32) * conversion
+        celsiusAmount = ((farenAmount - 32)*conversion)
         
         let tempFormatter = NSNumberFormatter()
         tempFormatter.numberStyle=NSNumberFormatterStyle.NoStyle
-        let temp = tempFormatter.stringFromNumber(farenAmount)
+        let temp = tempFormatter.stringFromNumber(celsiusAmount)
         celsius.text = temp! + " °C"
     }
     
@@ -231,11 +184,11 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     //function that will call function once user is out of textfield
     
+    //Managing text fields: https://grokswift.com/uitextfield/ 
+    
     func textFieldDidEndEditing(textField: UITextField) {
-        if(textField == dollars){
-            dollarsToPeso()
-        } else if (textField == pesos){
-            pesosToDollar()
+        if (textField == currencyRate){
+            updateDollars()
         } else if (textField == miles){
             milesToKm()
         } else if (textField == kilometers){
@@ -244,21 +197,25 @@ class ViewController: UIViewController, UITextFieldDelegate {
             farenToCelsius()
         } else if (textField == celsius) {
             celsiusToFaren()
+        } else if (textField == pounds){
+            poundsToKg()
+        } else if (textField == kilograms){
+            kgToPounds()
         }
     }
     
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        dollars.delegate = self //developer notes: handle text field's input via delegate callbacks
-        pesos.delegate = self
+        dollars.delegate = self //apple developer notes: handle text field's input via delegate callbacks
+        foreignCurrency.delegate = self
         kilometers.delegate = self
         miles.delegate = self
         celsius.delegate = self
         farenheit.delegate = self
         pounds.delegate = self
         kilograms.delegate = self
+        currencyRate.delegate = self
+        super.viewDidLoad()
     }
 
     override func didReceiveMemoryWarning() {
@@ -266,6 +223,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
         // Dispose of any resources that can be recreated.
     }
 
+    //CRED for arrows' picture: created by Sarah for the Noun Project
 
 }
 
